f1e988ce1288c9975c50538d1072cab3c88d6b47
ShivamGupta2746 <121252611+ShivamGupta2746@users.noreply.github.com>
2024-09-30 02:52:03 +0530

    Add files via upload

---
+++ Best Portfolio/script.js
@@ -0,0 +1,11 @@
+
+
+                        //  For Animation   
+
+
+var typed = new Typed(".typing",{
+    strings:["","Java Full Stack Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
---
+++ Best Portfolio/style-switcher.js
@@ -0,0 +1,49 @@
+
+                        // Toggle style switcher in personal portfolio  
+
+
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+
+                //   Hide style - switcher on scroll
+
+window.addEventListener("scroll", () => {
+    if(document.querySelector(".style-switcher").classList.contains("open")){
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+})
+
+
+                    //   Theme colors effect in personal portfolio
+
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color){
+    alternateStyles.forEach((style) => {
+        if(color === style.getAttribute("title")){
+            style.removeAttribute("disabled");
+        }
+        else{
+            style.setAttribute("disabled","true");
+        }
+    })
+}
+
+
+                        //  day and night for dark and light 
+
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+dayNight.querySelector("i").classList.toggle("fa-sun");
+dayNight.querySelector("i").classList.toggle("fa-moon");
+document.body.classList.toggle("dark");
+})
+window.addEventListener("load", () => {
+    if(document.body.classList.contains("dark")){
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else{
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+})
